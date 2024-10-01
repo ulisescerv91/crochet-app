fefb99a9f22af9bcd229153405b6e3d7117d01a1
Ulises Cervantes <ulises.cervantes@encora.com>
2024-09-30 22:00:52 -0600

    Simbolos-crochet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import CrochetTable from './components/CrochetTable';
 
 function App() {
+
+  const [puntos, setPuntos] = useState([]);
+
+  useEffect(() => {
+    fetch('/data/puntosCrochet.json')
+        .then((response) => response.json())
+        .then((data) => {
+            setPuntos(data);
+        })
+        .catch((error) => console.error('Error al cargar los datos:', error));
+}, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+            <header className="App-header">
+                <h1 style={{ textAlign: 'center', margin: '20px' }}>Crochet</h1>
+            </header>
+            <main>
+                <CrochetTable puntos={puntos} />
+            </main>
+            <footer>
+                <p>&copy; 2024 Crochet App</p>
+            </footer>
     </div>
   );
 }
---
+++ src/components/CrochetTable.js
@@ -0,0 +1,73 @@
+// src/components/CrochetTable.js
+import React from 'react';
+import ReactModal from 'react-modal';
+import './CrochetTable.css';
+
+ReactModal.setAppElement('#root'); // Importante para accesibilidad
+
+const ITEMS_PER_PAGE = 10;
+
+const CrochetTable = ({ puntos }) => {
+
+    return (
+        <>
+            <table className="crochet-table">
+                <thead>
+                    <tr>
+                     <th>Simbolo</th>
+                        <th>Nombre en Inglés</th>
+                        <th>Abreviatura</th>
+                        <th>Nombre en Español</th>
+                        <th> Tutorial <span className="bandera" style={{ fontSize: '1.5em',display: 'block' }}>🇬🇧</span></th>
+                        <th> Tutorial <span className="bandera" style={{ fontSize: '1.5em',display: 'block' }}>🇲🇽</span></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {puntos.map((punto, index) => (
+                        <tr key={index} style={{ cursor: 'pointer' }}>
+                            <td>{
+                                (index+1)<50 &&
+                                <img
+                                    src={`/images/${index+1}.png`}
+                                    alt={punto.nombreEspanol}
+                                    className="crochet-image"
+                                    loading="lazy"
+                                />
+                                }
+                            </td>
+                            <td>{punto.nombreIngles}</td>
+                            <td>{punto.abreviatura}</td>
+                            <td>{punto.nombreEspanol}</td>
+                            <td>
+                                {
+                                    <a
+                                        href={`https://www.youtube.com/results?search_query=${encodeURIComponent(punto.nombreIngles + ' crochet')}`}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        onClick={(e) => e.stopPropagation()}
+                                    >
+                                        <i className="fa fa-search" aria-hidden="true"></i> Videos
+                                    </a>
+                                }
+                            </td>
+                            <td>
+                                {
+                                    <a
+                                        href={`https://www.youtube.com/results?search_query=${encodeURIComponent(punto.nombreEspanol + ' crochet')}`}
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        onClick={(e) => e.stopPropagation()}
+                                    >
+                                        <i className="fa fa-search" aria-hidden="true"></i> Videos
+                                    </a>
+                                }
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
+};
+
+export default CrochetTable;
